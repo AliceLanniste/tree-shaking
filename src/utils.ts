0d73f5740179d8b9f7d7d303d1b5f1ea8ff80bfd
Alice Peng <1399789151@qq.com>
2024-09-19 09:58:35 +0800

    feat: add normlaizeModule

--- src/utils.ts
+++ src/utils.ts
@@ -1,12 +1,21 @@
-// TODO:
-// TODO:2.fetchModule完成
-
 import { resolve } from 'node:path';
 import { rainbowOptions } from './options';
+
 export function normalizeOptions(options: rainbowOptions) {
     let resolvePath: string[] = [];
     if(options.input) {
         resolvePath =Object.values(options.input).map((s) => resolve(s.replace(/\.js$/, '') + '.js'))
     }
     return resolvePath;
+}
+
+export function normalizeModule(entryPoints: Record<string,string>) {
+
+    return Object.entries(entryPoints).map(([name, id]) => ({
+		fileName: null,
+		id,
+		importer: undefined,
+		name
+	}));
+
 }
