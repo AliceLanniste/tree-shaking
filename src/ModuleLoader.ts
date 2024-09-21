098d27091afa045b04b6945ae83ca1ce75491e87
Alice Peng <1399789151@qq.com>
2024-09-21 11:19:42 +0800

    feat(ModuleLoader): add fetchMethod

--- src/ModuleLoader.ts
+++ src/ModuleLoader.ts
@@ -1,43 +1,71 @@
-import { type rainbowOptions } from "./types/options";
+import { NomlaizedResolveIdWithoutDefaults, ResolvedId, type rainbowOptions } from "./types/options";
 import { Module } from "./Module";
 import { type UnresolvedModule } from "./types/modules";
+import { EMPTY_OBJECT, resolveId } from "./utils";
+import { Graph } from "./Graph";
 export class ModuleLoader {
     
-     constructor(private readonly modulesById: Map<string, Module>,
+     constructor(
+        private readonly graph: Graph,
+        private readonly modulesById: Map<string, Module>,
         private readonly options: rainbowOptions,
      ) {
 
      }
 
-    addEntryModule(unresolveModules:UnresolvedModule[], isUserDefined: boolean) {
-       
+    async addEntryModule(unresolveModules:UnresolvedModule[], isUserDefined: boolean) {
+        const newEntryModules = await Promise.all(unresolveModules.map(({id, importer}) => 
+                               this.loadEntryModule(id,true,importer)))
     }
 
-  
-     
 	private async loadEntryModule(
 		unresolvedId: string,
 		isEntry: boolean,
 		importer: string | undefined,
 	): Promise<Module> {
+      const resolveResult = resolveId(unresolvedId,importer)
+        if (resolveResult === null) {
+        //    return;
+        }
 
+        return this.fetchModule(
+           this.getResolveIdWithResult({id: resolveResult!}, EMPTY_OBJECT)!,
+               undefined,
+               isEntry,
+        )
 
     }
   
-    private fetchModule() {
-
+    private async fetchModule({ attributes, id, syntheticNamedExports }: ResolvedId,
+		importer: string | undefined,
+		isEntry: boolean):Promise<Module> {
+         const existingModule = this.modulesById.get(id);
+         if(existingModule) {
+            return existingModule;
+         }
+		const module = new Module(
+			this.graph,
+			id,
+			this.options,
+			isEntry,
+			syntheticNamedExports,
+			attributes
+		);
+		this.modulesById.set(id, module);
+        return module;
     }
 
-    private getResolveIdWithResult(resolvedId:any, attributes:Record<string,string>) {
+    private getResolveIdWithResult(resolvedId: NomlaizedResolveIdWithoutDefaults | null, 
+                                    attributes:Record<string,string>): ResolvedId | null {
         if (!resolvedId) {
             return null;
         }
-
+        const external = resolvedId.external || false;
         return {
             attributes:resolvedId.attributes || attributes,
+            external,
             id:resolvedId.id,
-            meta:  {},
-			resolvedBy: resolvedId.resolvedBy ?? 'rollup',
+			resolvedBy: resolvedId.resolveBy ?? 'rollup',
 			syntheticNamedExports: resolvedId.syntheticNamedExports ?? false
         }
     }
