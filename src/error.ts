6a49ca926179b6df656629929989405f1f97d084
Alice Peng <1399789151@qq.com>
2024-09-25 22:10:03 +0800

    feat: add enum type about error

--- src/error.ts
+++ src/error.ts
@@ -10,3 +10,20 @@ export function error( errObj: ErrorType):never {
 
     throw Error(errMsg)
 }
+
+
+/**TODO: 1. resolveId(importee,importer)
+ * @param：importee:import的库
+ * @param: importer:import库引入到的模块
+ * @return:id： 通过importer,importee找到目标模块文件位置
+ * 
+ * TODO:2.将得到的id传入到	fetchModule ( id:string, importer:string|undefined )
+ * fetchModule的逻辑是
+ * load(id): 将文件加载读取为source
+ * transform(source)，转成transformResult{code,ast}
+ * 将transformResult转成Module，然后读取dependencies
+ */
+
+export enum ErrCode {
+    LODE_MODULE="LODE_MODULE"
+}
