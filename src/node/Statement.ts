9a50759562b7d290db933c3aa3c3f027c28cd04a
Alice Peng <1399789151@qq.com>
2024-10-04 12:05:19 +0800

    feat: add method

--- src/node/Statement.ts
+++ src/node/Statement.ts
@@ -1,12 +1,23 @@
 import { ModuleDeclaration, Statement as Acorn_Statement } from "acorn";
 import {Module} from "../Module";
+import MagicString from "magic-string";
 
 export class Statement {
-     
-    constructor(private readonly node:Acorn_Statement | ModuleDeclaration,
+    node:Acorn_Statement | ModuleDeclaration;
+    
+    constructor(node:Acorn_Statement | ModuleDeclaration,
         private readonly module: Module,
-        private readonly index: number
+        private readonly index: number,
+        private readonly magicString:MagicString,
     ){
-        
+       this.node = node;
     }
+
+    isImportDeclartion(): boolean {
+      return  this.node.type ==='ImportDeclaration'
+    }
+
+    isExportDeclartion(): boolean {
+        return  /^Export/.test( this.node.type )
+      }
 }
