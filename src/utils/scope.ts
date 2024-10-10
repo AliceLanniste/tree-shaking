b7e47a019776a6385252cb23e9812e6c0a5e550c
Alice Peng <1399789151@qq.com>
2024-10-10 10:58:43 +0800

    feat: add ScopOptionType

--- src/utils/scope.ts
+++ src/utils/scope.ts
@@ -1,22 +1,32 @@
+type scopeOptionType = {
+    parent?:Scope,
+    params?:string[],
+    isBlockScope?:boolean,
+}
+
 export default class Scope {
-    name: string[];
-    parent: Scope;
-    isBlockScope:boolean =false;
+    params: string[];
+    parent: Scope | undefined;
+    isBlockScope:boolean ;
+    options:scopeOptionType;
 
-    constructor() {
-
+    constructor(options:scopeOptionType ={}) {
+        this.options =options || {};
+        this.parent = this.options.parent;
+        this.params = this.options.params || [];
+        this.isBlockScope = this.options.isBlockScope || false;
     }
 
     add(name:string, isBlockDeclaration:boolean) {
-        if(this.isBlockScope && !isBlockDeclaration) {
+        if(this.isBlockScope && !isBlockDeclaration && this.parent) {
             this.parent.add(name,isBlockDeclaration);
         } else {
-            this.name.push(name);
+            this.params.push(name);
         }
     }
 
     contains(name:string):boolean {
-        if(this.name.includes(name)) return true;
+        if(this.params.includes(name)) return true;
 
         if (this.parent) return this.parent.contains(name);
 
