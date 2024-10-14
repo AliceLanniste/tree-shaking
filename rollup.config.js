23dc2631a84cc739e27675373489630ac551e981
Alice Peng <1399789151@qq.com>
2024-10-14 12:03:29 +0800

    chore: add plugins

--- rollup.config.js
+++ rollup.config.js
@@ -1,3 +1,8 @@
+import  typescript from '@rollup/plugin-typescript';
+import  commonjs from '@rollup/plugin-commonjs';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import json from '@rollup/plugin-json';
+
 export default {
     input:{
         'rollup.js': 'src/index.ts'
@@ -16,5 +21,14 @@ export default {
         interop: 'default',
         sourcemap: true
     },
+
+    plugins:[
+        nodeResolve(),
+        json(),
+        commonjs({
+            ignoreTryCatch: false,
+            include: 'node_modules/**'
+        }),
+        typescript()]
 	
 };
