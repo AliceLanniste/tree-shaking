b65fb316fe8b741528547ce27dc6083e27b91e95
Alice Peng <1399789151@qq.com>
2024-12-16 09:57:27 +0800

    feat: add plugin tests

---
+++ test/function/basic/.custom-path-resolver-async/_config.js
@@ -0,0 +1,34 @@
+var path = require( 'path' );
+var assert = require( 'assert' );
+
+module.exports = {
+	description: 'uses a custom path resolver (asynchronous)',
+	options: {
+		plugins: [{
+			resolveId: function ( importee, importer ) {
+				var resolved;
+
+				if ( path.normalize(importee) === path.resolve( __dirname, 'main.js' ) ) return importee;
+
+				if ( importee === 'foo' ) {
+					resolved = path.resolve( __dirname, 'bar.js' );
+				} else {
+					resolved = false;
+				}
+
+				return Promise.resolve( resolved );
+			}
+		}]
+	},
+	warnings: [
+		{
+			code: 'UNRESOLVED_IMPORT',
+			importer: 'main.js',
+			source: 'path',
+			message: `'path' is imported by main.js, but could not be resolved – treating it as an external dependency`,
+		}
+	],
+	exports: function ( exports ) {
+		assert.strictEqual( exports.path, require( 'path' ) );
+	}
+};
---
+++ test/function/basic/custom-path-resolver-sync/_config.js
@@ -0,0 +1,31 @@
+// var path = require( 'path' );
+// var assert = require( 'assert' );
+import path from 'node:path';
+import assert from 'assert';
+
+module.exports = {
+	description: 'uses a custom path resolver (synchronous)',
+	options: {
+		plugins: [{
+			resolveId: function ( importee, importer ) {
+				if ( path.normalize(importee) === path.resolve( __dirname, 'main.js' ) ) return importee;
+				if ( importee === 'foo' ) return path.resolve( __dirname, 'bar.js' );
+
+				return false;
+			}
+		}]
+	},
+	warnings: [
+		{
+			code: 'UNRESOLVED_IMPORT',
+			importer: 'main.js',
+			source: 'path',
+			message: `'path' is imported by main.js, but could not be resolved – treating it as an external dependency`,
+			url: `https://github.com/rollup/rollup/wiki/Troubleshooting#treating-module-as-external-dependency`
+		}
+	],
+	exports: function ( exports ) {
+		assert.strictEqual( exports.path, require( 'path' ) );
+    },
+    solo:true
+};
