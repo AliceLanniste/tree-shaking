86e51ce9b21382bcceaa13038364c4f2962c6e13
Alice Peng <1399789151@qq.com>
2024-12-16 20:23:42 +0800

    feat:  add loader plugin

--- src/Graph.ts
+++ src/Graph.ts
@@ -1,18 +1,32 @@
 import  * as MagicString from "magic-string";
 import { Module } from "./Module";
 import { ModuleLoader } from "./ModuleLoader";
-import { type rainbowOptions } from "./types";
-import { normalizeModules } from "./utils/utils";
+import { type rainbowOptions , type Plugin, SourceDescription, WarningHandler} from "./types";
+import { normalizeModules, sequence ,load} from "./utils/utils";
 export class Graph {
     readonly moduleLoader:ModuleLoader;
     modulesById:Record<string, Module> = {};
     names: Record<string, any> = {};
+    plugins: Plugin[];
+    onwarn: WarningHandler;
+    load: (id: string) => Promise<SourceDescription | string | void>;
     constructor(
         private readonly options: rainbowOptions,
     ) {
-        this.moduleLoader = new ModuleLoader(this,options);        
+        // this.plugins = options.plugins || [];
+        this.initPlugin(options)
+        this.moduleLoader = new ModuleLoader(this, options);
+            
+        
 
     } 
+
+    initPlugin(options: rainbowOptions) {
+        this.plugins = options.plugins || [];
+        const loaders = this.plugins.map(plugin => plugin.load).filter(Boolean)
+        this.load = sequence(loaders.concat(load))
+
+    }
 
      createModuleGraph(){ 
        return this.generateModuleGraph()
