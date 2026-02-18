2f8d37017a61713dc692d27766b85d5d2d28888b
AliceLanniste <1399789151@qq.com>
2026-02-18 16:58:55 +0800

    feat: add tsconfig.json

--- src/error.ts
+++ src/error.ts
@@ -4,7 +4,10 @@ export interface ErrorType {
 }
 
 export enum ERR_CODE {
-    PARSE_ERROR ='PARSE_ERROR'
+    PARSE_ERROR ='PARSE_ERROR',
+    DUPLICATE_IMPORT= 'DDUPLICATE_IMPORT',
+    DUPLICATE_EXPORT= 'DUPLICATE_EXPORT',
+    DUPLICATE_EXPORT_DEFAULT= 'DUPLICATE_EXPORT_DEFAULT',
 }
 
 export function error(errObj: ErrorType): never {
