f4431605e60fc31f8c5a0f140a2a81d6a236543d
AliceLanniste <1399789151@qq.com>
2026-02-19 21:51:07 +0800

    feat: add util functions

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import Graph from "./Graph";
+
+export default function rainbowUp(inputOptions: any) {
+   const graph = new Graph(inputOptions);
+
+    let chunks = graph.build();
+    //generate()
+   
+}
+
+function  generate(outputOptions: any, isWrite:boolean) {
+
+}
+
+
+
--- src/utils/util.ts
+++ src/utils/util.ts
@@ -1,13 +1,35 @@
-import { Program } from "../node";
-import { GenericNode } from "../node/shared";
+import { lstatSync, readdirSync, readFileSync } from "fs";
+import { basename, dirname, isAbsolute, resolve } from "path";
 
-export const STORE_KEYS: {[name: string]: string[]} = {
-    Program: ['body'],
+export function resolveId(id: string, importer?: string): string | null {
+    if(importer !== undefined && !isAbsolute(id) && id[0] !== '.') return null;
+    return addJsExtensionIfNecessary(resolve(importer ? dirname(importer) : resolve(), id))
 }
 
-export function getNodeKeys(node:GenericNode) {
-    STORE_KEYS[node.type] = Object.keys(node).filter(
-        key => typeof node[key] === 'object'
-    )
-    return STORE_KEYS[node.type]
+export function load(id: string) {
+    return readFileSync(id, 'utf-8');
+}
+
+export function transform(code: string) {
+    return code;
+}
+
+function findFile(file: string): string | void {
+    const stats = lstatSync(file);
+    if (stats.isFile()) {
+        const name = basename(file);
+        const files = readdirSync(dirname(file));
+
+        if (files.indexOf(name) != -1) return file;
+    }
+}
+
+function addJsExtensionIfNecessary(file: string):string {
+    let found = findFile(file);
+    if(found)  return found;
+    found = findFile(file + '.mjs');
+    if(found)  return found;
+    found = findFile(file + '.js');
+    if(found)  return found;
+
 }
