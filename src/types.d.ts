2ddfb3a8dae1d1b3c9e51d7d39ef276101ead94f
AliceLanniste <1399789151@qq.com>
2026-02-20 22:36:21 +0800

    feat: add new typings

--- src/types.d.ts
+++ src/types.d.ts
@@ -21,3 +21,30 @@ export interface  RainbowOutput{
     generate(outputOptions: OutputOptions, isWrite:boolean): Promise<RainbowOutput>;
     write(outputOptions: OutputOptions): Promise<RainbowOutput>;
 }
+
+export interface BaseLogProps {
+    pos?: number;
+    id?: string;
+    frame?: string;
+    loc?:{
+        file:string,
+        line: number,
+        column: number
+    },
+    message?:string,
+}
+
+export  interface RainbowError extends BaseLogProps {
+}
+export interface Warning extends BaseLogProps {
+    code: string;
+    exporter?: string;
+    exportName?: string;
+    importer?: string;
+    missing?: string;
+    modules?: string[];
+    names?: string[];
+    reexporter?: string;
+    source?: string;
+    sources?: string[];
+}
