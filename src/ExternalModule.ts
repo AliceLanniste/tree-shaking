b7b9add78d9d1ca313a57a94f29af75afc800ed8
AliceLanniste <1399789151@qq.com>
2026-02-23 22:04:50 +0800

    feat: add ExternalModule

--- src/ExternalModule.ts
+++ src/ExternalModule.ts
@@ -1,3 +1,14 @@
+import Graph from "./Graph";
+
 export default class ExternalModule {
+    private graph: Graph;
+    id: string;
     isExternal: boolean = true;
+    isEntrtyPoint: boolean = false;
+    execIndex: number;
+
+    constructor(id: string, graph: Graph) {
+        this.id = id;
+        this.graph = graph;
+    }
 }
