09ba739a7e52108680efc07fc86b18cebe4cb615
AliceLanniste <1399789151@qq.com>
2026-02-26 23:39:41 +0800

    feat: add preRender,render

--- src/Chunk.ts
+++ src/Chunk.ts
@@ -1,7 +1,11 @@
+import { ERR_CODE, error } from "./error";
 import ExternalModule from "./ExternalModule";
+import finalisers from "./finalisers";
 import Graph from "./Graph";
 import Module from "./Module";
+import { OutputOptions } from "./types";
 import Variable from "./variables/Variable";
+import MagicString, { Bundle as MagicStringBundle } from 'magic-string';
 
 export default class Chunk {
     execIndex: number;
@@ -10,7 +14,9 @@ export default class Chunk {
     graph: Graph;
     orderedModules: Module[];
     usedModules?: Module[];
-
+    
+    private renderedModuleSources: MagicString[];
+    private renderedSource: MagicStringBundle;
     private exports = new Set<Variable>();
     private imports = new Set<Variable>();
     private dependencies?: (Chunk | ExternalModule)[];
@@ -38,6 +44,44 @@ export default class Chunk {
         }
 
         this.dependencies= Array.from(dependencies);
+    }
+
+    preRender() {
+        const magicStringBundle = new MagicStringBundle( {separator: '\n\n'});
+        this.renderedModuleSources = []
+
+        for(let i = 0; i < this.orderedModules.length; i++){
+            const module = this.orderedModules[i];
+            const source = module.render({});
+            source.trim();
+            
+            if (source.lastLine().indexOf('//') !== -1) source.append('\n');
+            this.renderedModuleSources.push(source);
+
+            if (!source.isEmpty()) magicStringBundle.addSource(source);
+            this.renderedSource = magicStringBundle.trim();
+
+        }
+    }
+
+    render(options: OutputOptions) {
+        if (!this.renderedSource) 
+            throw new Error("Internal error: Chunk render called before preRender");
+
+        const finalise = finalisers[options.format];
+
+        if (!finalise) {
+            error({
+                code: ERR_CODE.INVALID_OPTION,
+                message: `Invalid format: ${options.format} - valid options are ${Object.keys(
+					finalisers
+				).join(', ')}`
+            })
+        }
+
+        const magicString = finalise(this.renderedSource, );
+        const prevCode = magicString.toString();
+        return prevCode;
     }
 
     private addChunksFromDependencies(moduleDependencies: (Module | ExternalModule)[],
